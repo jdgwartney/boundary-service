869c2d2079d380fadd239e42788e19ddec5e04ad
jdgwartney <davidg@boundary.com>
2014-06-16 09:24:11 -0700

    Initial checkin of boundary-sdk-service database

---
+++ README.md
@@ -0,0 +1,15 @@
+Boundary SDK Database
+=====================
+
+
+
+Pre-Install
+----------
+
+1) Install MySQL
+
+2) Create database besdk as root
+
+3) Add user "boundary" to the database"
+
+4) Grant permissions to the boundary database to besdk
---
+++ src/main/resources/sql/init.sql
@@ -0,0 +1,8 @@
+-- Create the Boundary SDK Database
+-- CREATE DATABASE IF NOT EXISTS besdk CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+-- Create the Boundary User
+CREATE USER 'boundary'@'localhost' IDENTIFIED BY 'boundary';
+
+-- Grant priviledges to the boundary user
+GRANT ALL PRIVILEGES ON besdk.* TO 'boundary'@'localhost';
