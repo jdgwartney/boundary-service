e8a146143eb3f1cfda7401ce26bbb7686b9f41ad
jdgwartney <davidg@boundary.com>
2014-06-16 15:27:24 -0700

    Add generated schema ; change init.sql to support ; add Workbench design file

--- db/src/main/resources/sql/init.sql
+++ db/src/main/resources/sql/init.sql
@@ -1,8 +1,8 @@
 -- Create the Boundary SDK Database
--- CREATE DATABASE IF NOT EXISTS besdk CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE DATABASE IF NOT EXISTS services CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 -- Create the Boundary User
 CREATE USER 'boundary'@'localhost' IDENTIFIED BY 'boundary';
 
 -- Grant priviledges to the boundary user
-GRANT ALL PRIVILEGES ON besdk.* TO 'boundary'@'localhost';
+GRANT ALL PRIVILEGES ON services.* TO 'boundary'@'localhost';
