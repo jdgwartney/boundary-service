bcb17283808515519bd4e435a3fca515ea40eef7
jdgwartney <davidg@boundary.com>
2014-07-22 15:09:34 -0700

    Add flags of enabling of service checks ; fix service data

--- db/src/main/resources/db/data.sql
+++ db/src/main/resources/db/data.sql
@@ -50,42 +50,37 @@ SET @url_test = LAST_INSERT_ID();
 INSERT INTO t_service(id,name,owner) VALUES(NULL,"SDN Director","alert@somewhere.com");
 SET @sdn_director_service = LAST_INSERT_ID();
 
-
-                                 
-
-
-
-
 --
 -- Define the Service Tests
 --
+SET @ip_address="192.168.137.11";
 
 INSERT INTO t_service_test(id,name,service_test_type_id) VALUES(NULL,'Check SDN Director host status',@ping_test);
 SET @service_test_sdn_director_ping_host = LAST_INSERT_ID();
 
 -- Ping Configuration Details
-INSERT INTO t_ping_config(id,service_test_id,host) VALUES(NULL,@service_test_sdn_director_ping_host,'192.168.137.11');
+INSERT INTO t_ping_config(id,service_test_id,host) VALUES(NULL,@service_test_sdn_director_ping_host,@ip_address);
 
 -- Check the status of the plumgrid process
 INSERT INTO t_service_test(id,name,service_test_type_id) VALUES(NULL,'Check plumgrid process status',@ssh_test);
 SET @service_test_sdn_director_ssh_plumgrid = LAST_INSERT_ID();
 
 INSERT INTO t_ssh_config(id,service_test_id,host,user,password,command,expected_output)
-VALUES(NULL,@service_test_sdn_director_ssh_plumgrid,'192.168.137.11','plumgrid','plumgrid','sudo status plumgrid','^plumgrid start/running, process\\s+(\\d+)\n');
+VALUES(NULL,@service_test_sdn_director_ssh_plumgrid,@ip_address,'plumgrid','plumgrid','sudo status plumgrid','^plumgrid start/running, process\\s\\d+');
 
 -- Check the status of the plumgrid-sal process
 INSERT INTO t_service_test(id,name,service_test_type_id) VALUES(NULL,'Check plumgrid-sal process status',@ssh_test);
 SET @service_test_sdn_director_ssh_plumgrid_sal = LAST_INSERT_ID();
 
 INSERT INTO t_ssh_config(id,service_test_id,host,user,password,command,expected_output)
-VALUES(NULL,@service_test_sdn_director_ssh_plumgrid_sal,'192.168.137.11','plumgrid','plumgrid','sudo status plumgrid-sal','^plumgrid-sal start/running, process\\s+(\\d+)\n');
+VALUES(NULL,@service_test_sdn_director_ssh_plumgrid_sal,@ip_address,'plumgrid','plumgrid','sudo status plumgrid-sal','^plumgrid-sal start/running, process\\s\\d+');
 
 -- Check the status of the nginx process
 INSERT INTO t_service_test(id,name,service_test_type_id) VALUES(NULL,'Check nginx process status',@ssh_test);
 SET @service_test_sdn_director_ssh_nginx = LAST_INSERT_ID();
 
 INSERT INTO t_ssh_config(id,service_test_id,host,user,password,command,expected_output)
-VALUES(NULL,@service_test_sdn_director_ssh_nginx,'192.168.137.11','plumgrid','plumgrid','sudo status nginx','^nginx start/running, process\\s+(\\d+)\n');
+VALUES(NULL,@service_test_sdn_director_ssh_nginx,@ip_address,'plumgrid','plumgrid','sudo status nginx','^nginx start/running, process\\s+\\d+');
 
 --
 -- Define the Service Check
@@ -98,7 +93,6 @@ SET @service_check_sdn_director = LAST_INSERT_ID();
 --
 INSERT INTO t_service_to_check(id,service_id,service_check_id)
 VALUES(NULL,@sdn_director_service,@service_check_sdn_director);
-
 
 --
 -- Relate Service Tests with the Service Check
@@ -122,11 +116,11 @@ VALUES(NULL,@service_check_sdn_director,@service_test_sdn_director_ssh_nginx);
 -- #    # #    # #   ##    #    # # 
 --  ####  #####  #    #     ####  # 
 
-INSERT INTO t_service VALUES(NULL,"SDN UI","alert@somewhere.com");
-SET @sdn_ui_service = LAST_INSERT_ID();
+-- INSERT INTO t_service VALUES(NULL,"SDN UI","alert@somewhere.com");
+-- SET @sdn_ui_service = LAST_INSERT_ID();
 
-INSERT INTO t_service_test(id,name,service_test_type_id) VALUES(NULL,'Check SDN UI host status',@ping_test);
-SET @service_test_sdn_ui_ping_host = LAST_INSERT_ID();
+-- INSERT INTO t_service_test(id,name,service_test_type_id) VALUES(NULL,'Check SDN UI host status',@ping_test);
+-- SET @service_test_sdn_ui_ping_host = LAST_INSERT_ID();
 
 -- INSERT INTO t_service VALUES(NULL,"Hypervisor","alert@somewhere.com");
 -- SET @service_id = LAST_INSERT_ID();
@@ -158,27 +152,27 @@ SET @service_test_sdn_ui_ping_host = LAST_INSERT_ID();
 -- ### #       #     # #     # 
 
 -- Add Service
-INSERT INTO t_service VALUES(NULL,"IPAM","alert@somewhere.com");
-SET @service_id = LAST_INSERT_ID();
+-- INSERT INTO t_service VALUES(NULL,"IPAM","alert@somewhere.com");
+-- SET @service_id = LAST_INSERT_ID();
 
 -- Add Service Check
-INSERT INTO t_service_check(id,name,schedule_id) VALUES(NULL,"IPAM Service Check",@schedule_every_minute);
-SET @service_check_id = LAST_INSERT_ID();
+-- INSERT INTO t_service_check(id,name,schedule_id) VALUES(NULL,"IPAM Service Check",@schedule_every_minute);
+-- SET @service_check_id = LAST_INSERT_ID();
 
 -- Relate Service To Service Check
-INSERT INTO t_service_to_check(id,service_id,service_check_id)
-VALUES(NULL,@service_id,@service_check_id);
+-- INSERT INTO t_service_to_check(id,service_id,service_check_id)
+-- VALUES(NULL,@service_id,@service_check_id);
 
 -- 1) Create service test
 -- 2) Relate service test to service check
 -- 2) Create service test configuration details
-INSERT INTO t_service_test(id,name,service_test_type_id) VALUES(NULL,'Check IPAM host status',@ping_test);
-SET @service_test_ping_host = LAST_INSERT_ID();
+-- INSERT INTO t_service_test(id,name,service_test_type_id) VALUES(NULL,'Check IPAM host status',@ping_test);
+-- SET @service_test_ping_host = LAST_INSERT_ID();
 
-INSERT INTO t_service_check_to_test(id,service_check_id,service_test_id)
-VALUES(NULL,@service_check_id,@service_test_ping_host);
+-- INSERT INTO t_service_check_to_test(id,service_check_id,service_test_id)
+-- VALUES(NULL,@service_check_id,@service_test_ping_host);
 
-INSERT INTO t_ping_config(id,service_test_id,host) VALUES(NULL,@service_test_ping_host,'192.168.137.11');
+-- INSERT INTO t_ping_config(id,service_test_id,host) VALUES(NULL,@service_test_ping_host,'192.168.137.11');
 
 
 -- INSERT INTO t_service VALUES(NULL,"IPAM GUI");
